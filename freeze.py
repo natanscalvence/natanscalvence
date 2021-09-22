2374b37cd421acc47563bc662411835367343ed8
natanscalvence <natanskern@protonmail.com>
2021-09-22 15:38:24 -0300

    Second Commit

---
+++ freeze.py
@@ -0,0 +1,15 @@
+from flask_frozen import Freezer
+from app import app, pages
+from flask import url_for
+
+app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME']
+freezer = Freezer(app)
+
+@freezer.register_generator
+def pagelist():
+    for page in pages:
+        print(f"making page for {page.path}")
+        yield url_for('page', path=page.path)
+
+if __name__ == "__main__":
+    freezer.freeze()
